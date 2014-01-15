d971e2389bd0fbd91a7404ecc22faeaf5cd86181
Philip Chu <games@fugugames.com>
2014-01-15 19:27:43 +0800

    updated for unity 4.3

--- AdBanner.cs
+++ AdBanner.cs
@@ -7,7 +7,7 @@ public class AdBanner : MonoBehaviour {
 		
 	public bool showOnTop = true;
 	public string AdMobID = "";
-#if P31_IAD
+#if UNITY_IPHONE && FUGU_ADS && P31_IAD
 	void Start() {
 		AdBinding.createAdBanner (!showOnTop);
 	}
@@ -15,19 +15,17 @@ public class AdBanner : MonoBehaviour {
 #if UNITY_IPHONE && FUGU_ADS && !P31_IAD
 	private ADBannerView banner = null;
 		
-	IEnumerator Start() {
+	void Start() {
 		GameObject.DontDestroyOnLoad(gameObject); // keep ad alive if we load a new scene
-		banner = new ADBannerView();
-		banner.autoSize = true;
-		banner.autoPosition = showOnTop ? ADPosition.Top : ADPosition.Bottom;
-		while (!banner.loaded && banner.error == null) {
-			yield return null;
+		banner = new ADBannerView(ADBannerView.Type.Banner,showOnTop ? 
+			                          ADBannerView.Layout.Top : ADBannerView.Layout.Bottom);
+		ADBannerView.onBannerWasLoaded  += OnBannerLoaded;
 		}
-		if (banner.error == null) {
-			banner.Show();
-		} else {
-			banner = null;
-		}
+
+		void OnBannerLoaded()
+		{
+			Debug.Log("Ad banner Loaded!\n");
+			banner.visible = true;
 		}
 #endif
 #if UNITY_ANDROID && FUGU_ADS

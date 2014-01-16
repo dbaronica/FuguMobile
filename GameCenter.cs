88930db641c6f5810675a09a387fa6893371fe03
Philip Chu <games@fugugames.com>
2014-01-16 10:02:46 +0800

    added prime31 support

--- GameCenter.cs
+++ GameCenter.cs
@@ -9,7 +9,7 @@ namespace Fugu {
 		public bool showAchievementBanners = true;
 		// Use this for initialization
 		void Start () {
-		#if UNITY_IPHONE
+#if UNITY_IPHONE && !P31_GC
 		Social.localUser.Authenticate ( success => {
       	  if (success && showAchievementBanners) {
 				GameCenterPlatform.ShowDefaultAchievementCompletionBanner(showAchievementBanners);
@@ -19,18 +19,24 @@ namespace Fugu {
 				Debug.Log ("Failed to authenticate "+Social.localUser.userName);
 			}
 		}
-    );
+    	);
+#endif
+#if UNITY_IPHONE && P31_GC
+			GameCenterBinding.authenticateLocalPlayer();
+			if (showAchievementBanners) {
+				GameCenterBinding.showCompletionBannerForAchievements();
+			}
 #endif
 	}
 		
 	static public void Achievement(string name) {
-			Achievement(name,100.0d);
+			Achievement(name,100.0f);
 		}
 		
-	static public void Achievement(string name,double score) {
-#if UNITY_IPHONE
-	if (Social.localUser.authenticated) {
-		Social.ReportProgress(name,score, success => {
+	static public void Achievement(string name, float progress) {
+		if (IsAuthenticated ()) {
+#if UNITY_IPHONE && !P31_GC
+			Social.ReportProgress(name,progress, success => {
 #if FUGU_DEBUG
 			if (success) {
 				Debug.Log("Achievement "+name+" reported successfully");
@@ -39,14 +45,17 @@ namespace Fugu {
 			}
 #endif
 		});
-	}
 #endif
+#if UNITY_IPHONE && P31_GC
+			GameCenterBinding.reportAchievement(name,progress);
+#endif
+		}
 }
 
 	static public void Score(string name,long score) {
-#if UNITY_IPHONE
-	if (Social.localUser.authenticated) {
-		  Social.ReportScore (score, name, success => {
+			if (IsAuthenticated ()) {
+#if UNITY_IPHONE && !P31_GC
+			  Social.ReportScore (score, name, success => {
 #if FUGU_DEBUG
 			if (success) {
 				Debug.Log("Posted "+score+" on leaderboard "+name);
@@ -55,9 +64,55 @@ namespace Fugu {
 			}
 #endif
 		});
-	}
 #endif
+#if UNITY_IPHONE && P31_GC
+				GameCenterBinding.reportScore(score, name);
+#endif
+			}
 }
+
+	static public bool IsAuthenticated() {
+#if !UNITY_IPHONE
+			return false;
+#endif
+#if UNITY_IPHONE && !P31_GC
+			return Social.localUser.authenticated;
+#endif
+#if UNITY_IPHONE && P31_GC
+			return GameCenterBinding.isPlayerAuthenticated();
+#endif
+		}
+
+			static public string GetName() {
+			if (!IsAuthenticated ()) {
+				return "Player";
+			} else {
+#if UNITY_IPHONE && !P31_GC
+				return Social.localUser.userName;
+#endif
+				#if UNITY_IPHONE && P31_GC
+				return GameCenterBinding.playerAlias();
+				#endif
+			}
+		}
+
+		static public void ShowLeaderboard() {
+#if UNITY_IPHONE && !P31_GC
+			Social.ShowLeaderboardUI ();
+#endif
+#if UNITY_IPHONE && P31_GC
+			GameCenterBinding.showLeaderboardWithTimeScope (GameCenterLeaderboardTimeScope.AllTime);
+#endif
+		}
+
+		static public void ShowAchievements() {
+			#if UNITY_IPHONE && !P31_GC
+			Social.ShowAchievementsUI ();
+			#endif
+			#if UNITY_IPHONE && P31_GC
+			GameCenterBinding.showAchievements ();
+			#endif
+		}
 	
 }
 }
